f29bda4b4b5b5e41a8d1150155ed6960db867962
anushka-2002 <85661914+anushka-2002@users.noreply.github.com>
2022-02-27 19:45:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,4 +12,29 @@ function accepted_friends()
     btn.style.backgroundColor="pink";
     let btnacc= document.getElementById("requested_button");
     btnacc.style.backgroundColor="var(--color-light)";
+
+    
+    function dropdownM1Function() {
+    document.getElementById("Month1").classList.toggle("show");
+}
+
+function dropdownM2Function() {
+    document.getElementById("Month2").classList.toggle("show");
+}
+
+function dropdownCatFunction() {
+    document.getElementById("Category").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
 }
